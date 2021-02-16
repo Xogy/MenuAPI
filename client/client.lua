13a6a67ace7e012bb5bc8ebbbc45ed7c05de7286
MrXogos <49158267+Xogy@users.noreply.github.com>
2021-02-16 16:01:27 +0100

    Removed test code

--- client/client.lua
+++ client/client.lua
@@ -36,38 +36,38 @@ RegisterKey(function()
 end, "menuapiescaper", "Key backspace", "back")
 --------------
 
-CreateThread(function()
-    Wait(200)
-    local vehicles = {
-        [1] = { label = "police car", model = "police"},
-        [2] = { label = "A FOCKING TANK?!", model = "rhino"},
-        [3] = { label = "a nice car!", model = "cerberus3"}
-    }
-
-    local menu = CreateMenu("identifier")
-
-    menu.SetMenuTitle("Vehicle spawner")
-
-    menu.SetProperties({
-        float = "right",
-        position = "middle",
-    })
-
-    for k,v in ipairs(vehicles) do
-        menu.AddItem(k, v.label, nil, { model = v.model })
-    end
-
-    menu.OnSelectEvent(function(index, data)
-        RequestModel(data.model)
-        local coords = GetEntityCoords(PlayerPedId())
-        while not HasModelLoaded(data.model) do
-            Wait(33)
-        end
-        local vehicle = CreateVehicle(data.model, coords.x, coords.y, coords.z, GetEntityHeading(PlayerPedId()), true, true)
-        SetVehicleOnGroundProperly(vehicle)
-        SetModelAsNoLongerNeeded(data.model)
-        TaskWarpPedIntoVehicle(PlayerPedId(), vehicle, -1)
-    end)
-
-    menu.Open()
-end)
+--CreateThread(function()
+--    Wait(200)
+--    local vehicles = {
+--        [1] = { label = "police car", model = "police"},
+--        [2] = { label = "A FOCKING TANK?!", model = "rhino"},
+--        [3] = { label = "a nice car!", model = "cerberus3"}
+--    }
+--
+--    local menu = CreateMenu("identifier")
+--
+--    menu.SetMenuTitle("Vehicle spawner")
+--
+--    menu.SetProperties({
+--        float = "right",
+--        position = "middle",
+--    })
+--
+--    for k,v in ipairs(vehicles) do
+--        menu.AddItem(k, v.label, nil, { model = v.model })
+--    end
+--
+--    menu.OnSelectEvent(function(index, data)
+--        RequestModel(data.model)
+--        local coords = GetEntityCoords(PlayerPedId())
+--        while not HasModelLoaded(data.model) do
+--            Wait(33)
+--        end
+--        local vehicle = CreateVehicle(data.model, coords.x, coords.y, coords.z, GetEntityHeading(PlayerPedId()), true, true)
+--        SetVehicleOnGroundProperly(vehicle)
+--        SetModelAsNoLongerNeeded(data.model)
+--        TaskWarpPedIntoVehicle(PlayerPedId(), vehicle, -1)
+--    end)
+--
+--    menu.Open()
+--end)
